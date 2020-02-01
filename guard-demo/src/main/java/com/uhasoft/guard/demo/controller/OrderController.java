d892717880057dc585c5dd0924ea5fc16f9c192d
Weihua <vip_wangweihua@163.com>
2020-02-01 17:13:13 +0800

    更新Demo，方便上手测试

---
+++ guard-demo/src/main/java/com/uhasoft/guard/demo/controller/OrderController.java
@@ -0,0 +1,44 @@
+package com.uhasoft.guard.demo.controller;
+
+import com.uhasoft.guard.annotation.PermissionType;
+import com.uhasoft.guard.annotation.Resource;
+import com.uhasoft.guard.annotation.Retrieve;
+import com.uhasoft.guard.context.UserThreadLocal;
+import com.uhasoft.guard.demo.entity.Order;
+import com.uhasoft.guard.demo.entity.Response;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Weihua
+ * @since 1.0.0
+ */
+@Resource("order")
+@RequestMapping("order")
+@RestController
+public class OrderController extends BaseController<Order> {
+
+  @GetMapping("id/{id}")
+  public Response<List<String>> findById(@PathVariable String id){
+    return Response.success(UserThreadLocal.getLimitation());
+  }
+
+  @Retrieve
+  @PostMapping("search")
+  public Response<List<String>> search(@RequestBody Map<String, String> searchBean){
+    return Response.success(UserThreadLocal.getLimitation());
+  }
+
+  /**
+   * 自定义PermissionType
+   * @param id 订单ID
+   * @return 订单
+   */
+  @PermissionType(type = "deliver")
+  @PutMapping("id/{id}")
+  public Response<List<String>> deliver(@PathVariable String id){
+    return Response.success(UserThreadLocal.getLimitation());
+  }
+}
---
+++ guard-demo/src/main/java/com/uhasoft/guard/demo/entity/Order.java
@@ -0,0 +1,66 @@
+package com.uhasoft.guard.demo.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @author Weihua
+ * @since 1.0.0
+ */
+public class Order {
+
+  private String id;
+  private String owner;
+  private BigDecimal amount;
+  private String area;
+  private String address;
+  private String status;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getOwner() {
+    return owner;
+  }
+
+  public void setOwner(String owner) {
+    this.owner = owner;
+  }
+
+  public BigDecimal getAmount() {
+    return amount;
+  }
+
+  public void setAmount(BigDecimal amount) {
+    this.amount = amount;
+  }
+
+  public String getArea() {
+    return area;
+  }
+
+  public void setArea(String area) {
+    this.area = area;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
